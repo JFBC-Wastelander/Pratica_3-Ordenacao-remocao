81e03e4c77bec95cf3de31a129774bfaee40bedb
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-18 16:54:13 -0300

    refactor: used prettier

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let numeros = [2, 1, 2, 3, 5, 3, 4, 3, 6, 7, 10, 9, 8, 7, 10, 1]
+let numeros = [2, 1, 2, 3, 5, 3, 4, 3, 6, 7, 10, 9, 8, 7, 10, 1];
 
 for (let i = 0; i < numeros.length; i++) {
-    for (let j = i + 1; j < numeros.length; j++) {
-        if (numeros[i] > numeros[j]) {
-            let aux = numeros[i]
-            numeros[i] = numeros[j]
-            numeros[j] = aux
-        } else if (numeros[i] == numeros[j]){
-            numeros.splice(j, 1)
-            j--
-        }
+  for (let j = i + 1; j < numeros.length; j++) {
+    if (numeros[i] > numeros[j]) {
+      let aux = numeros[i];
+      numeros[i] = numeros[j];
+      numeros[j] = aux;
+    } else if (numeros[i] == numeros[j]) {
+      numeros.splice(j, 1);
+      j--;
     }
+  }
 }
-console.log(numeros)
+console.log(numeros);
