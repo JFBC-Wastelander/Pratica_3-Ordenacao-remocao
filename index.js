c6279b232a115c9ecfc59c8a5227e559bad9e50c
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-18 16:48:09 -0300

    added: removed same values

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let numeros = [2, 1, 2, 3, 5, 3, 4, 3, 6, 7, 10, 9, 8, 7, 10, 1]
 
 for (let i = 0; i < numeros.length; i++) {
-    console.log("No primeiro laço:" + numeros[i])
     for (let j = i + 1; j < numeros.length; j++) {
-        console.log("No segundo laço:" + numeros[j])
         if (numeros[i] > numeros[j]) {
             let aux = numeros[i]
             numeros[i] = numeros[j]
             numeros[j] = aux
+        }
+    }
+}
+console.log(numeros)
+for (let i = 0; i < numeros.length; i++) {
+    for (let j = i + 1; j < numeros.length; j++) {
+        if (numeros[i] == numeros[j]) {
+            numeros.splice(j, 1)
+            j--
         }
     }
 }
