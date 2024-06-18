7f9d973c16b2c92e094d2fd4729c0d2ba6678345
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-18 16:52:38 -0300

    refactor: added ordened and removed same values in one single for, for

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ for (let i = 0; i < numeros.length; i++) {
             let aux = numeros[i]
             numeros[i] = numeros[j]
             numeros[j] = aux
+        } else if (numeros[i] == numeros[j]){
+            numeros.splice(j, 1)
+            j--
         }
     }
 }
 console.log(numeros)
-for (let i = 0; i < numeros.length; i++) {
+
+/*for (let i = 0; i < numeros.length; i++) {
     for (let j = i + 1; j < numeros.length; j++) {
         if (numeros[i] == numeros[j]) {
             numeros.splice(j, 1)
@@ -18,4 +22,4 @@ for (let i = 0; i < numeros.length; i++) {
         }
     }
 }
-console.log(numeros)
+console.log(numeros)*/
